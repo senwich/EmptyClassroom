168b075febad21a2e6305bab89e8d75b35bdd413
Simon Chen <3045409255@qq.com>
2026-06-11 18:00:53 +0800

    wip

--- worker/app/footer.tsx
+++ worker/app/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="ad-support-bar">
         <span className="ad-support-icon" aria-hidden="true">📣</span>
         <span>
-          为了本项目可持续运营，此处以极低价格进行研究生招生广告招租。其他合作也可致信森林电波实验室：
+          为了本项目可持续运营，此处低价招租进行研究生招生广告。合作请致电
           <a href="mailto:chengensen@foxmail.com">chengensen@foxmail.com</a>或微信senwich30454
         </span>
       </div>
